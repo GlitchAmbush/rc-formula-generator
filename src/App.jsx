9c7f72b54aee94aeaa723bdcfb3e79c3b40ca98c
GlitchAmbush <lemmytheleo@yahoo.com>
2023-03-19 12:03:07 -0400

    Crafting + Tools. WIP: Tools/Skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { useState } from 'react'
 import './App.css'
+import { toolOptions, skillOptions } from './resources/options'
 
 function App() {
+  // Sets/States
   const [name, setName] = useState()
   const [assistant, setAssistant] = useState([{ assistant: '' }])
   const [displayAssistant, setDisplayAssistant] = useState()
   const [itemName, setItemName] = useState()
   const [intention, setIntention] = useState()
-  const [tools, setTools] = useState()
+  const [tools, setTools] = useState([{tool: ''}])
 
+  // Functions
+  const makeOption = (item) => {
+    return <option key={item} value={item}>{item}</option>
+  }
+  
   const displayList = dictionary => {
     var list = ""
     var i = 0
-    
     for (var key of dictionary) {
       i++
       if (i == dictionary.length) {
@@ -21,7 +27,6 @@ function App() {
         list = list.concat(`${Object.values(key)}, `)
       }
     }
-
     return list
   }
   
@@ -31,13 +36,23 @@ function App() {
     setAssistant(data)
   }
 
+  const checkRadio = () => {
+    var getSelected = document.querySelector('input[name="intention"]:checked')
+    if (getSelected != null) {
+      if (getSelected.value == "") {
+        return document.getElementById('other-intention').value
+      }
+      
+      return getSelected.value
+    }
+  }
 
   const submit = (e) => {
     e.preventDefault()
     setName((name) => document.getElementById("name").value)
     setDisplayAssistant((displayAssistant) => displayList(assistant))
     setItemName((itemName) => document.getElementById("item").value)
-
+    setIntention((intention) => checkRadio())
   }
 
   const addAssistantFields = () => {
@@ -48,32 +63,31 @@ function App() {
   
   const removeFields = (index) => {
     let data = [...assistant]
-
     if (index != 0) {
       data.splice(index, 1)
       setAssistant(data)
     }
   }
 
+
+  // Main App
   return (
     <div className="App">
       <div className="row">
-
         <div className='col'>
-          <div className='form-container'>
+          <div>
             <p className='header'>Your Character Name</p>
-            <input placeholder='Character Name' id="name"></input>
+            <input className='text-input' placeholder='Character Name' id="name" autoComplete='off' />
           </div>
 
-          <div className='form-container'>
+          <div>
             <p>Research Assistant Name(s)</p>
-            
             {assistant.map((input, index) => {
               if (index == 0) {
                 return (
                   <div key={index}>
                   <input
-                    className="assistant-input"
+                    className="text-input modular-input"
                     name="assistant"
                     placeholder='Assistant Name'
                     value={input.assistant}
@@ -86,7 +100,7 @@ function App() {
               return (
                 <div key={index}>
                   <input
-                    className="assistant-input"
+                    className="text-input modular-input"
                     name="assistant"
                     placeholder='Assistant Name'
                     value={input.assistant}
@@ -99,11 +113,45 @@ function App() {
             <button className="add-input" onClick={addAssistantFields}>Add Assistant</button>
           </div>
 
-          <div className='form-container'>
+          <div>
             <p>Item Being Researched</p>
-            <input placeholder='Item Name' id="item"></input>
+            <input className='text-input' placeholder='Item Name' id="item" />
           </div>
 
+          <div>
+            <p>Crafting Intention</p>
+            <input type="radio" name="intention" id="one-off" value="One-Off" /> One-Off <br/>
+            <input type="radio" name="intention" id="patent" value="Patent" /> Patent <br/>
+            <input type="radio" name="intention" id="sell" value="Sell / Quantity" /> Sell / Quantity <br/>
+            <input type="radio" name="intention" id="public" value="Public Use" /> Public Use <br/>
+            <input type="radio" name="intention" id="other" value="" /> Other:<input className='small-input' id='other-intention' /><br />
+          </div>
+
+          <div>
+            <p>Tools</p>
+            <select className="text-input modular-input" id="tools">{toolOptions.map(makeOption)}</select><br />
+            <select className="text-input modular-input" id="tools">{skillOptions.map(makeOption)}</select><br />
+            
+            {tools.map((input, index) => {
+              if (index == 0) {
+                return (
+                  <div key={index}>
+                    <select className="text-input modular-input" id="tools" value={input.tools}>{toolOptions.map(makeOption)}</select>
+                  </div>
+                )
+              }
+
+              return (
+                <div key={index}>
+                  <select className="text-input modular-input" id="tools" value={input.tools}>{toolOptions.map(makeOption)}</select>
+                  <button className='remove'>Remove</button>
+                </div>
+              )
+            })}
+
+            <button className="add-input">Add Tool</button>
+            <button className="add-input" style={{marginLeft: "5px"}}>Add Skill</button>
+          </div>
           <button onClick={submit}>Update</button>
         </div>
 
@@ -115,7 +163,7 @@ function App() {
           <i>Your Character Name:</i> {name} <br />
           <i>Name of Research Assistant:</i> {displayAssistant}<br />
           <i>Item being researched:</i> {itemName}<br />
-          <i>Craft Intention:</i> <br />
+          <i>Craft Intention:</i> {intention}<br />
           <br />
           <b>FORMULA</b> <br />
           <u>Tools</u> <br />
@@ -127,8 +175,6 @@ function App() {
             <li>#### gp (Material flavor)</li>
           </ul>
 
-          <br />
-
           <u>Directions</u><br />
           <ol>
             <li>Example of crafting directions.</li>
@@ -136,8 +182,6 @@ function App() {
             <li>Make it relate to the materials!</li>
             <li>Add your little special twists, it can be more steps.</li>
           </ol>
-
-          <br />
 
           <b>Item Name</b><br />
           <i>Item Type, Rarity (requires attunement?)</i><br />
---
+++ src/resources/options.jsx
@@ -0,0 +1,28 @@
+export const toolOptions = [
+    "Select Tool",
+    "Alchemist's supplies",
+    "Brewer's supplies",
+    "Calligrapher's supplies",
+    "Carpenter's tools",
+    "Cartographer's tools",
+    "Cobbler's tools",
+    "Cook's utensils",
+    "Glassblower's tools",
+    "Jeweler's tools",
+    "Leatherworker's tools",
+    "Mason's tools",
+    "Painter's supplies",
+    "Potter's tools",
+    "Smith's tools",
+    "Tinker's tools",
+    "Weaver's tools",
+    "Woodcarver's tools",
+]
+
+export const skillOptions = [
+    "Select Skill",
+    "Arcana",
+    "Medicine",
+    "Nature",
+    "Religion"
+]
